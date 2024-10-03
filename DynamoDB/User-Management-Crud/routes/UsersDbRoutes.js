9f7ee0f1625d17a79430545c4880ed9a37fec63b
benjaminkors <128436815+benjaminkors@users.noreply.github.com>
2024-10-03 12:35:54 -0400

    Update UsersDbRoutes.js

--- DynamoDB/User-Management-Crud/routes/UsersDbRoutes.js
+++ DynamoDB/User-Management-Crud/routes/UsersDbRoutes.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const userController = require("../controllers/UsersDbController");
 
+const router = express.Router();
+
+router.post("/create", userController.createUser);
+router.get("/users", userController.getUsers);
+router.get("/read", userController.readUser);
+router.put("/update", userController.updateUser);
+router.delete("/delete", userController.deleteUser);
+
+module.exports = router;
